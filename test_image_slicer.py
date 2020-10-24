ff0c31cfc38219dc43ec361f988bd5c86f9daba2
dinisov <dgokaydin@gmail.com>
2020-10-24 11:41:57 +1000

    Changed way images is sliced and the size is calculated

--- test_image_slicer.py
+++ test_image_slicer.py
@@ -3,7 +3,7 @@ from PIL import Image
 
 img = Image.open('./image_slicer/test/sample.jpg')
 
-tiles = image_slicer.slice('./image_slicer/test/sample.jpg',col=5,row=5)
+tiles = image_slicer.slice('./image_slicer/test/sample.jpg', col=10, row=7, save=False)
 
 size = image_slicer.get_combined_size(tiles)
 
